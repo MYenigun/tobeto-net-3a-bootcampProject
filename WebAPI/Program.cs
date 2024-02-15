4f5ed049208423e2fcb7cf86504d7939c3e39250
ZafranBlue <enriq@ZAFRANBLUE>
2024-02-16 00:32:28 +0300

    Add project files.

---
+++ WebAPI/Program.cs
@@ -0,0 +1,26 @@
+using DataAccess;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        builder.Services.AddDataAccessServices(builder.Configuration);
+
+        var app = builder.Build();
+
+
+
+        // Configure the HTTP request pipeline.
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
